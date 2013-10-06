a47d3a7d244eea179e4bf7766409fbcd8132fd18
analyticsPierce <jason@marketingscience.co>
2013-10-06 15:02:29 -0600

    adding the app framework

---
+++ art-culture-history/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtCultureHistory::Application.config.secret_key_base = 'cc4e0025b8472b51c9e7f3376333e1c0baf30ba82dd32175a929e33293c0d44c6c19ce7c3b7e3672f81982e319714423ac75dc6b5e3964c1775c8d0072d8efb4'
---
+++ art-culture-history/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArtCultureHistory::Application.config.session_store :cookie_store, key: '_art-culture-history_session'
